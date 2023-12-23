c352736f967319c6d98f5b6bfe9d5e534dc8429f
Adam Lipok <lipokadam98@gmail.com>
2023-12-23 23:57:44 +0100

    day 3

---
+++ day_3/control_flow.py
@@ -0,0 +1,8 @@
+height = int(input('Enter height'))
+
+if 120 <= height < 130:
+    print('You are tall enough')
+elif height >= 130 <= 170:
+    print('middle height')
+else:
+    print('Grow some more')
