b889be49cf9e4facd152a99877e098e12cccfec3
Adam Lipok <lipokadam98@gmail.com>
2023-12-24 00:30:47 +0100

    day 4

--- day_4/random_numbers.py
+++ day_4/random_numbers.py
@@ -1,5 +1,12 @@
 import random
-
+import my_module
 random_int = random.randint(1,1101210)
+random_float = random.random()
 
 print(random_int)
+print(random_float)
+
+print(my_module.pi)
+
+random_float = random.random() * 5
+print(random_float)
